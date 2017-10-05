61b677a270abbe0d4bad0b5cbae378ab7fc70373
Dofas <32168254+Dofas@users.noreply.github.com>
2017-10-05 16:10:52 +0300

    Add files via upload

---
+++ lab7_5.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+
+b=input('Zadaite ryadok : ')
+
+a=str.lower(b)
+
+i=0
+
+g=0
+
+while i<len(a):
+	
+  if (a[i]=='a') or (a[i]=='o') or (a[i]=='u') or (a[i]=='i') or (a[i]=='e') or (a[i]=='y'):
+		
+    g +=1
+	
+  i=i+1
+
+
+
+print('Golosnux bykv ',g)
