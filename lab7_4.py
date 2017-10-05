8631d7e048faf70d97b4fdc447cadd183f694544
Dofas <32168254+Dofas@users.noreply.github.com>
2017-10-05 17:06:36 +0300

    Add files via upload

---
+++ lab7_4.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+def wifr(a):
+  
+  new=''
+  
+  for i in a:
+    
+    new=new+chr(ord(i)+1)
+  
+  return new
+
+a=input('zadaite r9dok ')
+
+print('Zashufrovanui r9dok : ',wifr(a))
