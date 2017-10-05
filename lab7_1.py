63374f7d67755671e440e50c4a9adf8779e5201d
Dofas <32168254+Dofas@users.noreply.github.com>
2017-10-05 17:39:37 +0300

    Add files via upload

---
+++ lab7_1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+
+b=input('Zadaite ryadok : ')
+
+c=b[::]
+
+k=input('Zadaite dovzuny zsyvy')
+
+if int(k)>len(b)/2:
+   
+   print('k ne moze bytu bil6wum za polovuny r9dka')
+
+new=''
+
+new=c[k+1:len(c)]+c[0:k+1]
